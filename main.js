868cd00cdbab483dee9ffeb840a33e1b7001cb27
Jose Hans Bautista <Jazz.j.h.r@gmail.com>
2022-03-21 00:28:08 -0600

    Madeit
    Lo logre, pude hacer que los botones cambien con el mouseover y mouseout

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 
 
 document.addEventListener("DOMContentLoaded", function () { 
-    var boton = document.querySelector(".button");
-    
-    boton.addEventListener('mouseover', function(){
-      boton.style.background = "#f0f8ff";
-      boton.style.setProperty("border-radius", "10%"); 
-      boton.style.setProperty("color", "black");
-    });
-    boton.addEventListener('mouseout', function(){
-      boton.style.setProperty("background-color", "rgba(240, 248, 255, 0)");
-      boton.style.setProperty("border.style", "none");
-      boton.style.setProperty("color", "white");
+    var boton = document.querySelectorAll("button#button1");
+    var botones = [document.querySelector("button#button1"), document.querySelector("button#button2"), document.querySelector("button#button3"), document.querySelector("button#button4")];
+  
+    botones.forEach((boton) => {
+      boton.addEventListener('mouseover', function(){
+        boton.style.setProperty("background-color", "#f0f8ff");
+        boton.style.setProperty("border-radius", "10%"); 
+        boton.style.setProperty("color", "#000000");
+      });
+      boton.addEventListener('mouseout', function(){
+        boton.style.setProperty("background-color", "rgba(240, 248, 255, 0)");
+        boton.style.setProperty("border.style", "none");
+        boton.style.setProperty("color", "white");
+      });
     });
 });
